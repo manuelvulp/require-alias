c94103ed7dda24fbe93d166df62523a4ae215a3f
Manuel Vulp <manuelvulp@gmail.com>
2014-08-04 16:34:31 +0300

    alias

---
+++ example/app.js
@@ -0,0 +1,76 @@
+var Alias = require('../lib/require-alias');
+
+/**
+ * In current descriptions context, ALIAS_ROOT points to path that is currently set as root in Alias library
+ * and ROOT points to the root of this project. Also do note that all directories are surrounded with {} just
+ * to avoid confusion
+ *
+ * -----------------------------------------------------------------------------------------------------------------
+ *
+ * By default ALIAS_ROOT points to your projects main file / runnable. Currently we are running {example/app.js} file
+ * which means our ALIAS_ROOT points to {ROOT/example/} folder. Say we wanted that all our aliases should be relative
+ * to absolute root, so we manually configure this.
+ */
+
+Alias.configure.root('../');
+
+/**
+ * Everything is now relative to {/example/../} aka {ROOT}
+ * Lets setup few aliases to point to some folders
+ */
+
+Alias.configure.paths({
+    '@foo': 'example/folders/foo', // {ROOT/example/folders/foo}
+    '@folders': 'example/folders' // {ROOT}/example/folders}
+});
+
+/**
+ * Require files and execute
+ */
+
+var foo = alias('@foo/file');
+var bar = alias('@folders/bar/file');
+
+foo(); // Output: 'Hello Foo'
+bar(); // Output: 'Hello Bar' 
+
+/**
+ * Often we need paths, not exports. To get paths, pass in second parameter as 'true' to alias
+ */
+
+var fooPath = alias('@foo/file', true);
+var barPath = alias('@folders/bar/file', true);
+
+console.log(fooPath); // Output: ./../example/folders/foo/file
+console.log(barPath); // Output: ./../example/folders/bar/file
+
+/**
+ * NB! If you haven't configured an alias for a path that you are trying to require, it will use the default require
+ * method. In other words:
+ *
+ * alias('folders/foo/file');
+ *    is
+ * require('folders/foo/file');
+ */
+
+var fooAlias = alias('./folders/foo/file');
+var fooRequire = require('./folders/foo/file');
+
+fooAlias(); // Output: 'Hello Foo'
+fooRequire(); // Output: 'Hello Foo'
+
+var barAlias = alias('../example/folders/bar/file');
+var barRequire = require('../example/folders/bar/file');
+
+barAlias(); // Output: 'Hello Bar'
+barRequire(); // Output: 'Hello Bar'
+
+/**
+ * Because no relative path is defined (./ or ../), Alias tries to require this file as ALIAS_ROOT + your_path
+ */
+ 
+var bazAlias = alias('example/baz'); 
+// var bazRequire = require('example/baz'); // Does not work
+
+bazAlias(); // Output: 'Hello Baz'
+// bazRequire(); // Output: throws error
